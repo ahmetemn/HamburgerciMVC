20b542e632fb9e5a3d2cda4a7cea211fd914d64d
antares <ahmet_emin_serttas@hotmail.com>
2024-03-11 09:44:02 +0300

    added menu

---
+++ HamburgerciMVC.DATA/Concrate/Menu.cs
@@ -0,0 +1,13 @@
+namespace HamburgerciMVC.DATA.Concrate
+{
+    public class Menu
+    {
+           public  string  Name  { get; set; }
+
+
+           
+           public  int   Age  { get; set; }
+
+
+    }
+}
