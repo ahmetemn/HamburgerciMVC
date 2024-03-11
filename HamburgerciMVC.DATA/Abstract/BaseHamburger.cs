ae6573dcf5973a5f401c5d0b34a7c3415e4ac840
antares <ahmet_emin_serttas@hotmail.com>
2024-03-11 09:35:13 +0300

    Added Data N-Tıer

---
+++ HamburgerciMVC.DATA/Abstract/BaseHamburger.cs
@@ -0,0 +1,23 @@
+﻿using HamburgerciMVC.DATA.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamburgerciMVC.DATA.Abstract
+{
+    public abstract class BaseHamburger
+    {
+
+        public  int Id  { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;   
+
+        public DateTime DeletedDate { get; set; }
+
+        public DateTime? UpdatedDate { get; set; } = null;
+
+        public Status Status { get; set; } = Status.Added; 
+    }
+}
